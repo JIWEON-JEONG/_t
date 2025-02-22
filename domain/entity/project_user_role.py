f201f2f0354cd2dccc0252420787a7aae448841b
onuii <richard@onuii.com>
2025-02-22 14:01:21 +0900

    test: 프로젝트 정책 기반 유닛테스트

--- domain/entity/project_user_role.py
+++ domain/entity/project_user_role.py
@@ -1,6 +1,6 @@
 from datetime import datetime, UTC
 from sqlalchemy import Column, Enum, Integer, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from domain.entity.enum import ProjectRole
 
--- domain/entity/user.py
+++ domain/entity/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime, UTC
 from sqlalchemy import Column, Enum, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 from domain.entity.enum import UserRole
 
